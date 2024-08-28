5960ae09e45b16b575182f02f73c9468b8c00134
charbut123 <charlyalmasih593@gmail.com>
2024-08-28 14:12:10 +0700

    Add files via upload

---
+++ Bab9soal3/src/main/java/com/mycompany/bab9soal3/Bab9soal3.java
@@ -0,0 +1,16 @@
+   /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab9soal3;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab9soal3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
