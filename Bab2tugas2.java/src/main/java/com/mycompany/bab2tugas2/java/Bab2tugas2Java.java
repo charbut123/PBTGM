e060dfbd39e82ca2689192b7661db8d3ed99bbf9
charbut123 <charlyalmasih593@gmail.com>
2024-08-28 13:56:22 +0700

    Add files via upload

---
+++ Bab2tugas2.java/src/main/java/com/mycompany/bab2tugas2/java/Bab2tugas2Java.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab2tugas2.java;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab2tugas2Java {
+
+    
+          
+  
+    public static String Makanan;           
+    public static final String Minuman = "Jus jambu";
+    
+    public static void main(String args) {
+        Makanan = "Ayam Geprek";
+        
+       
+        System.out.println("Makanan Favorit saya: "  +   Makanan);
+        System.out.println("Minuman Favorit saya: "  +   Minuman);
+        
+    }   
+}
+    
+
