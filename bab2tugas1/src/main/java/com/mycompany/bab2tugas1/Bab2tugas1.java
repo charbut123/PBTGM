d18298397fbb749865cbdcdebb4ec2bcf9fa5f61
charbut123 <charlyalmasih593@gmail.com>
2024-08-28 13:55:35 +0700

    Add files via upload

---
+++ bab2tugas1/src/main/java/com/mycompany/bab2tugas1/Bab2tugas1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab2tugas1;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab2tugas1 {
+
+    public static void main(String[] args) {
+         //Overflow
+        int a=50;
+        float b=a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+          
+       
+            
+   
