2d9eaf918a9e1c691fe19a09387d48ae784dc5b1
charbut123 <charlyalmasih593@gmail.com>
2024-08-28 14:00:07 +0700

    Add files via upload

---
+++ Bab5soal1java/src/main/java/com/mycompany/bab5soal1java/Bab5soal1java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab5soal1java;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab5soal1java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
